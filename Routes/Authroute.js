a6b4e85d4b84ab93f447da9ff371095facd84249
SureshKumar-24 <sk20012404@example.com>
2023-07-04 12:00:28 +0530

    brcpt password

--- Routes/Authroute.js
+++ Routes/Authroute.js
@@ -3,29 +3,26 @@ const router = express.Router();
 const User = require('../Models/User');
 const createError = require('http-errors');
 const { authSchema } = require('../helpers/validation_schema');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res, next) => {
     try {
-        const { email, password } = req.body;
+        const { email } = req.body;
         const result = await authSchema.validateAsync(req.body);
-        console.log(result);
-        if (!email || !password) {
-            return res.status(401).json({ msg: "Something wrong " })
-        }
 
-        const doesExist = await User.findOne({ email: email });
-        if (doesExist) {
-            return res.status(403).json({ msg: "email is already registerd" })
-        }
+        const doesExist = await User.findOne({ email: result.email });
+        if (doesExist) throw createError.Conflict(`${result.email} is already registered`);
 
+        const salt = await bcrypt.genSalt(10);
+        const password = await bcrypt.hash(result.email, salt);
         const user = new User({ email, password });
         const savedUser = await user.save();
-
         return res.send(savedUser);
+
     } catch (error) {
+        if (error.isJoi === true) error.status = 422
         next(error);
     }
-    // res.send('register route');
 })
 
 router.post('/login', async (req, res, next) => {
